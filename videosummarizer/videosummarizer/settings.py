35d89e279ac8c66e7268556fa20f3ab9d64662a4
AbhiMeliodus <abhi.sen11@rediffmail.com>
2025-09-18 11:11:18 +0530

    Updating settings file for huggingface spaces

--- videosummarizer/videosummarizer/settings.py
+++ videosummarizer/videosummarizer/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
+DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
